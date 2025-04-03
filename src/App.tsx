6c35f7a8bd75192fd08cce0c089b56bfc6eea354
Sahil <sahilverma1931@gmail.com>
2025-04-03 22:19:16 +0530

    Add boiler plate code for React typescript project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Typography} from '@mui/material'
+
+function App() {
+
+  return (
+    <>
+      <Typography>
+        helo hello
+      </Typography>
+    </>
+  )
+}
+
+export default App
